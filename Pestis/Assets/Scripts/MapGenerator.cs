36eb367b2a6bcc3866f30c26ba60103938d487f0
Shayaan Ahmed <117999566+shayaan-ahm3d@users.noreply.github.com>
2025-01-26 16:11:09 +0000

    closed holes in map

--- Pestis/Assets/Scripts/MapGenerator.cs
+++ Pestis/Assets/Scripts/MapGenerator.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 using ProceduralToolkit;
 using ProceduralToolkit.FastNoiseLib;
+using Math = System.Math;
 
 public class MapGenerator : MonoBehaviour
 {
@@ -10,19 +11,21 @@ public class MapGenerator : MonoBehaviour
     public int width;
     public int height;
     public List<TileBase> tiles;
-    public float frequency;
-    public int steps;
+    public float voronoiFrequency = 0.03f;
+    public int randomWalkSteps = 524288;
+    public int numberOfBiomes = 2;
 
     private FastNoise _noiseGenerator;
 
     private void Awake()
     {
-        bool[,] landWaterSplit = RandomWalk();
+        int[,] landWaterSplit = RandomWalk();
+        var map = Dilation(landWaterSplit);
         for (int x = 0; x < width; x++)
         {
             for (int y = 0; y < height; y++)
             {
-                if (landWaterSplit[x, y])
+                if (map[x, y] == 1)
                 {
                     tilemap.SetTile(new Vector3Int(x, y), tiles[0]);
                 }
@@ -38,7 +41,7 @@ public class MapGenerator : MonoBehaviour
     {
         _noiseGenerator = new FastNoise();
         _noiseGenerator.SetNoiseType(FastNoise.NoiseType.Cellular);
-        _noiseGenerator.SetFrequency(frequency);
+        _noiseGenerator.SetFrequency(voronoiFrequency);
 
         for (int x = 0; x < width; x++)
         {
@@ -58,18 +61,18 @@ public class MapGenerator : MonoBehaviour
     }
 
     // Credit: https://www.noveltech.dev/procgen-random-walk
-    public bool[,] RandomWalk()
+    public int[,] RandomWalk()
     {
         // create the grid, which will be filled with false value
         // true values define valid cells which are part of our visited map
-        bool[,] grid = new bool[width, height];
+        int[,] grid = new int[width, height];
 
         // choose a random starting point
         System.Random rnd = new System.Random();
         Vector2Int curr_pos = new Vector2Int(rnd.Next(width), rnd.Next(height));
 
         // register this position in the grid
-        grid[curr_pos.x, curr_pos.y] = true;
+        grid[curr_pos.x, curr_pos.y] = 1;
         //tilemap.SetTile(new Vector3Int(curr_pos.x, curr_pos.y), tiles[0]);
 
         // define allowed movements: left, up, right, down
@@ -82,7 +85,7 @@ public class MapGenerator : MonoBehaviour
         };
 
         // iterate on the number of steps and move around
-        for (int id_step = 0; id_step < steps; id_step++)
+        for (int id_step = 0; id_step < randomWalkSteps; id_step++)
         {
             // for each step, we try to find a new cell to go to.
             // We are not guaranteed to find a position that is valid (i.e. inside the grid)
@@ -96,7 +99,7 @@ public class MapGenerator : MonoBehaviour
                 if (new_pos.x >= 0 && new_pos.x < width && new_pos.y >= 0 && new_pos.y < height)
                 {
                     // this is a valid position, we set it in the grid
-                    grid[new_pos.x, new_pos.y] = true;
+                    grid[new_pos.x, new_pos.y] = 1;
                     //tilemap.SetTile(new Vector3Int(new_pos.x, new_pos.y), tiles[0]);
 
                     // replace curr_pos with new_pos
@@ -110,5 +113,157 @@ public class MapGenerator : MonoBehaviour
 
         return grid;
     }
+    public int[,] DiffusionAggregation()
+    {
+        int[,] grid = new int[width, height];
 
+        // fill with -1s to get the initial state 
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                grid[i, j] = -1;
+            }
+        }
+
+        // create a tracker for valid points by subsection for the diffusion process 
+        List<List<Vector2Int>> recorded_points = new();
+        for (int i = 0; i < numberOfBiomes; i++)
+        {
+            recorded_points.Add(new());
+        }
+
+        // create a counter to keep track of the number of unallocated cells 
+        int nb_free_cells = width * height;
+
+        // set up the initial points for the process 
+        System.Random rng = new();
+        for (int id_subsection = 0; id_subsection < numberOfBiomes; id_subsection++)
+        {
+            while (true)
+            {
+                // find a random point 
+                Vector2Int point = new(
+                    rng.Next(width),
+                    rng.Next(height)
+                );
+
+                // check if it's free, else find another point
+                if (grid[point.x, point.y] == -1)
+                {
+                    // if it is add it to tracking and grid then proceed to next subsection
+                    grid[point.x, point.y] = id_subsection;
+                    recorded_points[id_subsection].Add(point);
+                    nb_free_cells -= 1;
+
+                    break;
+                }
+            }
+        }
+
+        // Diffusion process moves from a given point to another point in a one cell movement so we're setting the directions here as a reusable element
+        Vector2Int[] directions = new Vector2Int[4] { Vector2Int.left, Vector2Int.up, Vector2Int.right, Vector2Int.down };
+
+        // now we can start filling the grid 
+        while (nb_free_cells > 0)
+        {
+            for (int id_subsection = 0; id_subsection < numberOfBiomes; id_subsection++)
+            {
+                // check if there are tracked points for this subsection 
+                if (recorded_points[id_subsection].Count == 0)
+                {
+                    continue;
+                }
+
+                // choose a random point from the tracked points 
+                int id_curr_point = rng.Next(recorded_points[id_subsection].Count);
+
+                Vector2Int curr_point = recorded_points[id_subsection][id_curr_point];
+
+                // choose a direction at random
+                Vector2Int new_point = curr_point + directions[rng.Next(4)];
+
+                // check if the new point is in the grid
+                if (new_point.x < 0 || new_point.y < 0 || new_point.x >= width || new_point.y >= height)
+                {
+                    continue;
+                }
+
+                // next check if the new point is already occupied and skip this direction if it is
+                if (grid[new_point.x, new_point.y] != -1)
+                {
+                    continue;
+                }
+
+                // else we can record this new point in our tracker and set it in the grid 
+                grid[new_point.x, new_point.y] = id_subsection;
+                recorded_points[id_subsection].Add(new_point);
+                nb_free_cells -= 1;
+
+            }
+        }
+
+        return grid;
+    }
+
+    private int[,] Closing(int[,] map)
+    {
+        return Erosion(Dilation(map));
+    }
+    
+    private int[,] Dilation(int[,] map)
+    {
+        int[,] result = new int[width, height];
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                int maxVal = map[x, y];
+                for (int i = -7; i <= 7; i++)
+                {
+                    for (int j = -7; j <= 7; j++)
+                    {
+                        int newX = x + i;
+                        int newY = y + j;
+                        if (newX >= 0 && newX < width && newY >= 0 && newY < height)
+                        {
+                            maxVal = Math.Max(maxVal, map[newX, newY]);
+                        }
+                    }
+                }
+                result[x, y] = maxVal;
+            }
+        }
+
+        return result;
+    }
+    
+    private int[,] Erosion(int[,] map)
+    {
+        int[,] result = new int[width, height];
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                int minVal = map[x, y];
+                for (int i = -5; i <= 5; i++)
+                {
+                    for (int j = -5; j <= 5; j++)
+                    {
+                        int newX = x + i;
+                        int newY = y + j;
+                        if (newX >= 0 && newX < width && newY >= 0 && newY < height)
+                        {
+                            minVal = Math.Min(minVal, map[newX, newY]);
+                        }
+                    }
+                }
+                result[x, y] = minVal;
+            }
+        }
+
+        return result;
+    }
 }
