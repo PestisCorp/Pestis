340d8cccc56537a1e4860d39e82b49cde0c0e872
AniBoja <aniboja26@gmail.com>
2025-01-27 18:19:03 +0000

    Can transition from N to N+k and N-k where k is in [1,2,3,4,5]. Added a nothing state (Transition from N to N)  which is equal to 1 - (birth_prob + death_prob).

--- Pestis/Assets/Scripts/Population/markov.jl
+++ Pestis/Assets/Scripts/Population/markov.jl
@@ -1,28 +1,39 @@
-const POP_MAX = 1000
+const POP_MAX = 20
 birth_rate = 0.2
 death_rate = 0.05
 resources = 100
 pop_size = 10
 
+
 function use_resources(population, food)
     return food / (food + population)
 end
 
-function α(b, F, N)
-    return b * use_resources(N, F)
+function α(b, F, N, w)
+    return b * use_resources(N, F) * w
 end
 
-function β(d, F, N)
-    return d * use_resources(N, F)
+function β(d, F, N, w)
+    return d * use_resources(N, F) * w
 end
 
 transition_matrix = zeros(Float64, POP_MAX, POP_MAX)
+W = [5, 4, 3, 2, 1]
+W_MIN = minimum(W)
+W_MAX = maximum(W)
 for i = 1:POP_MAX
-    for j = 1:POP_MAX
-        if i == j + 1
-            transition_matrix[i, j] = α(birth_rate, resources, pop_size)
-        elseif i == j - 1
-            transition_matrix[i, j] = β(death_rate, resources, pop_size)
+    σ = 1
+    for j = 1:5
+        if i + j > POP_MAX || i - j < 1
+            continue
         end
+        w = (W[j] - W_MIN) / (W_MAX - W_MIN)
+        αj = α(birth_rate, resources, pop_size, w)
+        βj = β(death_rate, resources, pop_size, w)
+        σ -= αj + βj
+        transition_matrix[i, i+j] = αj
+        transition_matrix[i, i-j] = βj
     end
+    transition_matrix[i, i] = σ
 end
+display(transition_matrix)
