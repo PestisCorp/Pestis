ac809d3a2d5d971d920d939de51d1241611c4164
AniBoja <aniboja26@gmail.com>
2025-01-27 20:15:45 +0000

    Runs an indefinite simulation of a population. Fixed some errors where probabilities weren't getting normalised, wasn't setting appropriate bounds, plus I shouldn't have set the loop to skip on boundary conditions.

--- Pestis/Assets/Scripts/Population/markov.jl
+++ Pestis/Assets/Scripts/Population/markov.jl
@@ -1,3 +1,5 @@
+using StatsBase
+
 const POP_MAX = 20
 birth_rate = 0.2
 death_rate = 0.05
@@ -17,23 +19,42 @@ function β(d, F, N, w)
     return d * use_resources(N, F) * w
 end
 
-transition_matrix = zeros(Float64, POP_MAX, POP_MAX)
-W = [5, 4, 3, 2, 1]
-W_MIN = minimum(W)
-W_MAX = maximum(W)
-for i = 1:POP_MAX
-    σ = 1
-    for j = 1:5
-        if i + j > POP_MAX || i - j < 1
-            continue
+function generate_transition_matrix(W, NMax, N, F, b, d)
+    transition_matrix = zeros(Float64, NMax, NMax)
+    W_MIN = minimum(W)
+    W_MAX = maximum(W)
+    for i = 1:NMax
+        for j = 1:5
+            w = (W[j] - W_MIN) / (W_MAX - W_MIN)
+            if i + j <= NMax
+                αj = α(b, F, N, w)
+                transition_matrix[i, i+j] = αj
+            end
+            if i - j >= 1
+                βj = β(d, F, N, w)
+                transition_matrix[i, i-j] = βj
+            end
         end
-        w = (W[j] - W_MIN) / (W_MAX - W_MIN)
-        αj = α(birth_rate, resources, pop_size, w)
-        βj = β(death_rate, resources, pop_size, w)
-        σ -= αj + βj
-        transition_matrix[i, i+j] = αj
-        transition_matrix[i, i-j] = βj
+        transition_matrix[i, i] = max(1 - sum(transition_matrix[i, :]), 0.0)
     end
-    transition_matrix[i, i] = σ
+    foreach(StatsBase.normalize!, eachrow(transition_matrix))
+    return transition_matrix
 end
-display(transition_matrix)
+
+function simulate_population(NMax, N, F, b, d)
+    W = [5, 4, 3, 2, 1]
+    transition_matrix = generate_transition_matrix(W, NMax, N, F, b, d)
+    while true
+        if N <= 0
+            println("Population has gone extinct.")
+            return
+        end
+        println("Population: ", N)
+        probs = transition_matrix[N, :]
+        next_state = StatsBase.sample(1:NMax, StatsBase.ProbabilityWeights(probs))
+        N = next_state
+        sleep(1)
+    end
+end
+
+simulate_population(POP_MAX, pop_size, resources, birth_rate, death_rate)
