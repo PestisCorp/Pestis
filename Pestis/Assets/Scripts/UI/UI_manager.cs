06d9c3ad3764c98b411f3e8310f00238128daa53
harveycald <jv21451@bristol.ac.uk>
2025-01-30 03:13:55 +0000

    Added functions that update UI text
    Functions are mostly placeholder as this isn't connected to the rest of the game yet, so default values are used.
    
    Working functions update the statistics and mutations in the info panel, own and enemy statistics in the attack panel and the slider max pop value visually and logically, as well as allowing dynamic display of the selected population value

--- Pestis/Assets/Scripts/UI/UI_manager.cs
+++ Pestis/Assets/Scripts/UI/UI_manager.cs
@@ -1,11 +1,19 @@
+using Horde;
+using Players;
 using UnityEngine;
 
 public class UI_Manager : MonoBehaviour
 {
+    // References to the game managers and objects to view in UI
+    // Temporarily set to null to prevent errors until it can be hooked up properly
+    private GameObject inputHandler = null;
+    private HumanPlayer localPlayer = null;
+
     // References to the canvas elements
     public GameObject infoPanel;
     public GameObject attackPanel;
     public GameObject mutationPopUp;
+    public GameObject toolbar;
 
     // Start is called before the first frame update
     void Start()
@@ -14,7 +22,7 @@ public class UI_Manager : MonoBehaviour
         ResetUI();
         if (mutationPopUp != null) mutationPopUp.SetActive(false);
     }
-    
+
     // Function to reset all referenced canvases to their default states to prevent UI clutter
     // Not including mutation Pop Up as this is not controlled by button presses
     public void ResetUI()
@@ -24,10 +32,37 @@ public class UI_Manager : MonoBehaviour
     }
 
     // Function to enable info panel
+    // and update the text fields
     public void EnableInfoPanel()
     {
         ResetUI();
         if (infoPanel != null) infoPanel.SetActive(true);
+
+        // Find all GameObjects with the tag "UI_stats_text"
+        GameObject[] taggedObjects = GameObject.FindGameObjectsWithTag("UI_stats_text");
+
+        // Loop through and find the one specific to the info panel with name "Info_own_stats"
+        foreach (GameObject obj in taggedObjects)
+        {
+            if (obj.name == "Info_own_stats")
+            {
+                HordeController horde = GetSelectedHorde();
+                UpdateStats(obj, horde);
+            }
+        }
+
+        // Find all GameObjects with the tag "UI_mutations_text"
+        GameObject[] taggedObjects2 = GameObject.FindGameObjectsWithTag("UI_mutations_text");
+
+        // Loop through and find the one specific to the info panel with name "Info_mutations"
+        foreach (GameObject obj in taggedObjects2)
+        {
+            if (obj.name == "Info_mutations")
+            {
+                HordeController horde = GetSelectedHorde();
+                UpdateMutations(obj, horde);
+            }
+        }
     }
 
     // Function to disable info panel
@@ -35,14 +70,45 @@ public class UI_Manager : MonoBehaviour
     {
         if (infoPanel != null) infoPanel.SetActive(false);
     }
-    
+
     // Function to enable attack panel
     public void EnableAttackPanel()
     {
         ResetUI();
         if (attackPanel != null) attackPanel.SetActive(true);
+
+        // Find all GameObjects with the tag "UI_stats_text"
+        GameObject[] uiStatsTextObjects = GameObject.FindGameObjectsWithTag("UI_stats_text");
+
+        // Loop through and find the one specific to the attack panel with name "Attack_own_stats"
+        foreach (GameObject obj in uiStatsTextObjects)
+        {
+            if (obj.name == "Attack_own_stats")
+            {
+                HordeController horde = GetSelectedHorde();
+                UpdateStats(obj, horde);
+            }
+            else if (obj.name == "Attack_enemy_stats")
+            {
+                HordeController horde = GetSelectedEnemyHorde();
+                UpdateStats(obj, horde);
+            }
+        }
+        
+        // Find all GameObjects with the tag "Attack_slider_text
+        GameObject[] attackSliderObjects = GameObject.FindGameObjectsWithTag("Attack_slider_text");
+
+        // Loop through and find the one specific to the attack panel with name "Attack_own_stats"
+        foreach (GameObject obj in attackSliderObjects)
+        {
+            if (obj.name == "Text_max_pop")
+            {
+                HordeController horde = GetSelectedHorde();
+                UpdateSliderMaxPop(obj, horde);
+            }
+        }
     }
-    
+
     // Function to disable attack panel
     public void DisableAttackPanel()
     {
@@ -60,4 +126,130 @@ public class UI_Manager : MonoBehaviour
     {
         if (mutationPopUp != null) mutationPopUp.SetActive(false);
     }
+
+    // Function to enable toolbar
+    public void EnableToolbar()
+    {
+        ResetUI();
+        if (toolbar != null) toolbar.SetActive(true);
+    }
+
+    // Function to disable toolbar
+    public void DisableToolbar()
+    {
+        if (toolbar != null) toolbar.SetActive(false);
+    }
+
+    //To display the correct UI the LocalPlayer will be monitored and when they select a horde the toolbar will be displayed
+    //The selected horde game object will also be acquired so the horde statistics can be retrieved and displayed
+
+    // Function to retrieve the selected horde and return it
+    private HordeController GetSelectedHorde()
+    {
+        return inputHandler?.GetComponent<InputHandler>()?.LocalPlayer?.selectedHorde;
+    }
+
+    // Function to retrieve the selected enemy horde and return it
+    private HordeController GetSelectedEnemyHorde()
+    {
+        return null;
+    }
+
+    // Function to update the stats text field of the passed game object
+    // Using the template:
+    // • Population: XX
+    // • Attack: XX
+    // • Defense: XX
+    // • Avg. Size: XXcm
+    // • Avg. Weight: XXKg
+    private void UpdateStats(GameObject statsText, HordeController horde)
+    {
+        if (horde != null)
+        {
+            // Create string variables for the stats, with XX as default if no value is present
+            string population = horde.AliveRats.ToString();
+            string attack = "XX";
+            string defense = "XX";
+            string avgSize = "XX";
+            string avgWeight = "XX";
+
+            string stats = "Population: " + population + "\n" +
+                           "Attack: " + attack + "\n" +
+                           "Defense: " + defense + "\n" +
+                           "Avg. Size: " + avgSize + "cm\n" +
+                           "Avg. Weight: " + avgWeight + "Kg";
+            statsText.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = stats;
+        }
+        else statsText.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = "No Horde Selected";
+    }
+
+    // Function to update the mutation text field of the info panel
+    // Using the template:
+    //Genome XX: 
+    //   Mutation XX - Description
+    //   Mutation XX - Description
+    // TODO: Implement this function
+    private void UpdateMutations(GameObject mutationText, HordeController horde)
+    {
+        if (horde != null)
+        {
+            mutationText.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = "No Genome found";
+        }
+        else mutationText.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = "No Horde Selected";
+    }
+
+    // Function to update the max population text of the attack horde size slider
+    // Both the text field and the slider max value will be updated
+    private void UpdateSliderMaxPop(GameObject maxPopText, HordeController horde)
+    {
+        //Get the slider with tag "Attack_slider"
+        GameObject slider = GameObject.FindGameObjectWithTag("Attack_slider");
+        
+        if (horde != null)
+        {
+            int population = horde.AliveRats;
+            
+            //Change the max value of the slider to the population of the horde
+            slider.GetComponent<UnityEngine.UI.Slider>().maxValue = population;
+            
+            maxPopText.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = population.ToString();
+        }
+        else
+        {
+            //Set default value to 100 if no horde is selected
+            slider.GetComponent<UnityEngine.UI.Slider>().maxValue = 100;
+            maxPopText.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = "100";
+        }
+        
+        //Set the value of the slider to the half of the max value (rounding down for odd numbers)
+        slider.GetComponent<UnityEngine.UI.Slider>().value = slider.GetComponent<UnityEngine.UI.Slider>().maxValue / 2;
+        UpdateSelectedPopulation();
+
+    }
+    
+    
+    // Function to update the currently selected population
+    public void UpdateSelectedPopulation()
+    {
+        
+        //Get the slider with tag "Attack_slider"
+        GameObject slider = GameObject.FindGameObjectWithTag("Attack_slider");
+        
+        //Get the current value of the slider
+        int selectedPopulation = (int)slider.GetComponent<UnityEngine.UI.Slider>().value;
+        
+        //Find all GameObjects with the tag "Attack_slider_text"
+        GameObject[] attackSliderObjects = GameObject.FindGameObjectsWithTag("Attack_slider_text");
+        
+        //Loop through and find the one specific to the attack panel with name "Attack_selected_pop"
+        foreach (GameObject obj in attackSliderObjects)
+        {
+            if (obj.name == "Text_selected_pop")
+            {
+                obj.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = selectedPopulation.ToString();
+            }
+        }
+    }
 }
+    
+    
