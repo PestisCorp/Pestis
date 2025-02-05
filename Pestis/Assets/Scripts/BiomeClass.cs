95dcd0bd3509b43fe2cb1ffd5aea012ca43773fc
VinhBui01 <15buiv@johnmasonschool.org>
2025-02-05 15:22:39 +0000

    slight oop change
    Changed oop structure to be more cohesive, now each biome can have their own overridden generation rules working on feature gen now

--- Pestis/Assets/Scripts/BiomeClass.cs
+++ Pestis/Assets/Scripts/BiomeClass.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -16,13 +17,25 @@ public class BiomeClass : ScriptableObject
     //in game stuff
 
 
+    void Shuffle<T>(T[] array)
+    {
+        System.Random rng = new System.Random(); // Use C# Random
+        int n = array.Length;
+        for (int i = n - 1; i > 0; i--)
+        {
+            int j = rng.Next(i + 1); // Pick a random index
+            (array[i], array[j]) = (array[j], array[i]); // Swap
+        }
+    }
+
     public BiomeTile getRandomBiomeTile()
     {
         int index = Random.Range(0, TileList.Length);
         return TileList[index];
     }
 
-    public GameObject getRandomFeature()
+
+public GameObject getRandomFeature()
     {
         int index = Random.Range(0, FeatureList.Length);
         return FeatureList[index];
@@ -33,24 +46,79 @@ public class BiomeClass : ScriptableObject
         // Empty method
     }
 
-    public virtual void Growth()//generate biomes by seeding 1 biomeTile and then repeatedly "growing" it
+
+    public virtual BiomeInstance sowSeed(Tilemap map)
     {
-        // Empty method
+        BoundsInt bounds = map.cellBounds;
+        bool success = false;
+        while (true)
+        {
+            int randomX = Random.Range(bounds.xMin, bounds.xMax);
+            int randomY = Random.Range(bounds.yMin, bounds.yMax);
+            Vector3Int randomPosition = new Vector3Int(randomX, randomY);
+            TileBase currentTile = (TileBase)map.GetTile(randomPosition);
+            // Check if the current tile is of a specific type (e.g., Grass)
+            if (CompatableTerrainTypes.Contains(currentTile))
+            {
+                map.SetTile(randomPosition, getRandomBiomeTile());
+                return (new BiomeInstance(this, new Vector2Int(randomX, randomY)));
+            }
+        }
+    }
+    public virtual void Growth(Vector2Int currentPosition, Tilemap map, BiomeInstance biomeInstance, int walkLength)//generate biomes by seeding 1 biomeTile and then repeatedly "growing" it
+    {
+         BoundsInt bounds = map.cellBounds;
+        // Perform the drunkard's walk
+        for (int i = 0; i < walkLength; i++)
+        {
+
+            // Pick a random direction (Up, Down, Left, Right)
+            Vector2Int[] directions = { Vector2Int.up, Vector2Int.down, Vector2Int.left, Vector2Int.right };
+            Shuffle(directions);
+            int valid = 0;
+            int index = 0;
+            while (valid == 0 && index < 4)
+            {
+                Vector2Int newPosition = currentPosition + directions[index];
+
+                if (biomeInstance.tilePositions.Contains(newPosition))
+                {
+                    valid = 1;
+                    currentPosition = newPosition;
+                }
+                else if (CompatableTerrainTypes.Contains((TileBase)map.GetTile(new Vector3Int(newPosition.x, newPosition.y, 0))))
+                {
+                    valid = 2;
+                    currentPosition = newPosition;
+                    map.SetTile(new Vector3Int(newPosition.x, newPosition.y, 0), getRandomBiomeTile());
+                    biomeInstance.tilePositions.Add(currentPosition);
+
+                }
+                index += 1;
+            }
+            if (valid == 0)
+            {
+                i = walkLength;
+            }
+            // Keep within map bounds
+            currentPosition.x = Mathf.Clamp(currentPosition.x, bounds.xMin, bounds.xMax);
+            currentPosition.y = Mathf.Clamp(currentPosition.y, bounds.yMin, bounds.yMax);
+        }
     }
 
-    public virtual void CellGeneration(Tilemap map)//generate biomes by seeding 1 biomeTile and then repeatedly "growing" it
+    public virtual void CellGeneration(Tilemap map, BiomeInstance biomeInstance)//generate biomes by seeding 1 biomeTile and then repeatedly "growing" it
     {
-        // Empty method
+        for (int i = 0; i < iteration; i++)
+        {
+            Growth(biomeInstance.seedPosition, map, biomeInstance, walkLength);
+        }
     }
+
 }
 public class BiomeInstance
 {
     public BiomeClass template;  // Reference to the template (BiomeClass)
     public Vector2Int seedPosition;
-    public int seedCount;
-    public int walkLength;
-    public int iteration;
-    public int strength;
 
     public List<Vector2Int> tilePositions = new List<Vector2Int>(); // Stores generated tiles for this biome
 
@@ -58,10 +126,6 @@ public class BiomeInstance
     {
         this.template = template;
         this.seedPosition = position;
-        this.seedCount = template.seedCount;
-        this.walkLength = template.walkLength;
-        this.iteration = template.iteration;
-        this.strength = template.strength;
     }
 
 }
--- Pestis/Assets/Scripts/BiomeGenerator.cs
+++ Pestis/Assets/Scripts/BiomeGenerator.cs
@@ -16,10 +16,7 @@ public class BiomeGenerator : MonoBehaviour
         this.sowSeed();
         foreach (var biome in BiomeList)
         {
-            for (int i = 0; i < biome.iteration; i++)
-            {
-                RandomWalk(biome.seedPosition, biome);
-            }
+            biome.template.CellGeneration(map, biome);
         }
     }
 
@@ -31,75 +28,11 @@ public class BiomeGenerator : MonoBehaviour
         {
             for (int j = 0; j < BiomeClasses[i].seedCount; j++)
             {
-                bool success = false;
-                while (success == false)
-                {
-                    int randomX = Random.Range(bounds.xMin, bounds.xMax);
-                    int randomY = Random.Range(bounds.yMin, bounds.yMax);
-                    Vector3Int randomPosition = new Vector3Int(randomX, randomY);
-                    TileBase currentTile = (TileBase)map.GetTile(randomPosition);
-                    // Check if the current tile is of a specific type (e.g., Grass)
-                    if (BiomeClasses[0].CompatableTerrainTypes.Contains(currentTile))
-                    {
-                        map.SetTile(randomPosition, BiomeClasses[i].getRandomBiomeTile());
-                        BiomeList.Add(new BiomeInstance(BiomeClasses[i], new Vector2Int(randomX, randomY)));
-                        success = true;
-                    }
-                }
+                BiomeList.Add(BiomeClasses[i].sowSeed(map));
             }
         }
 
     }
-    void Shuffle<T>(T[] array)
-    {
-        System.Random rng = new System.Random(); // Use C# Random
-        int n = array.Length;
-        for (int i = n - 1; i > 0; i--)
-        {
-            int j = rng.Next(i + 1); // Pick a random index
-            (array[i], array[j]) = (array[j], array[i]); // Swap
-        }
-    }
 
-    private void RandomWalk(Vector2Int currentPosition, BiomeInstance biome)
-    {
-        BoundsInt bounds = map.cellBounds;
-        // Perform the drunkard's walk
-        for (int i = 0; i < biome.walkLength; i++)
-        {
-
-            // Pick a random direction (Up, Down, Left, Right)
-            Vector2Int[] directions = { Vector2Int.up, Vector2Int.down, Vector2Int.left, Vector2Int.right };
-            Shuffle(directions);
-            int valid = 0;
-            int index = 0;
-            while (valid == 0 && index < 4)
-            {
-                Vector2Int newPosition = currentPosition + directions[index];
-
-                if (biome.tilePositions.Contains(newPosition))
-                {
-                    valid = 1;
-                    currentPosition = newPosition;
-                }
-                else if (biome.template.CompatableTerrainTypes.Contains((TileBase)map.GetTile(new Vector3Int(newPosition.x, newPosition.y, 0))))
-                {
-                    valid = 2;
-                    currentPosition = newPosition;
-                    map.SetTile(new Vector3Int(newPosition.x, newPosition.y, 0), biome.template.getRandomBiomeTile());
-                    biome.tilePositions.Add(currentPosition);
-
-                }
-                index += 1;
-            }
-            if (valid == 0)
-            {
-                i = biome.walkLength;
-            }
-            // Keep within map bounds
-            currentPosition.x = Mathf.Clamp(currentPosition.x, bounds.xMin, bounds.xMax);
-            currentPosition.y = Mathf.Clamp(currentPosition.y, bounds.yMin, bounds.yMax);
-        }
-    }
 }
 
