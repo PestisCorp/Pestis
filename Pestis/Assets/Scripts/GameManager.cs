fcda503b2eb4d76950586f637f6403edc4d62bc7
Murray Groves <github@murraygrov.es>
2025-02-27 11:35:26 +0000

    Add fps/boid debug count

--- Pestis/Assets/Scripts/GameManager.cs
+++ Pestis/Assets/Scripts/GameManager.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
+using System.Linq;
 using Map;
 using Players;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -15,9 +17,20 @@ public class GameManager : MonoBehaviour
     public Tilemap terrainMap;
     public List<Player> Players;
 
+    public TMP_Text fpsText;
+    public TMP_Text boidText;
+
+    private readonly float[] fpsWindow = new float[60];
+    private int fpsIndex;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
+#if UNITY_EDITOR
+        fpsText.gameObject.SetActive(true);
+        boidText.gameObject.SetActive(true);
+#endif
+
         if (Instance == null)
         {
             Instance = this;
@@ -30,5 +43,19 @@ public class GameManager : MonoBehaviour
 
         Application.targetFrameRate = 58;
         QualitySettings.vSyncCount = 0;
+    }
+
+    private void Update()
+    {
+#if UNITY_EDITOR
+        fpsWindow[fpsIndex] = Time.unscaledDeltaTime;
+        if (fpsIndex == 59)
+            fpsIndex = 0;
+        else
+            fpsIndex++;
+
+        fpsText.text = $"FPS: {60.0f / fpsWindow.Sum()}";
+        boidText.text = $"Boids: {Players.Sum(player => player.Hordes.Sum(horde => horde.AliveRats))}";
+#endif
     }
 }
