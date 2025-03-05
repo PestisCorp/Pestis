4edf0081e5afaf45adef01946ed5bb88a64ff9e3
VinhBui01 <15buiv@johnmasonschool.org>
2025-03-05 21:24:34 +0000

    minor tweak 2

--- Pestis/Assets/Scripts/BiomeClass.cs
+++ Pestis/Assets/Scripts/BiomeClass.cs
@@ -82,7 +82,7 @@ public class BiomeClass : ScriptableObject
         {
             Vector3 cityWorldPos = Vector3.zero;
             bool placedCity = false;
-            float minCityDistance = 30.0f; // required distance from any other city (global check)
+            float minCityDistance = 20.0f; // required distance from any other city (global check)
 
             foreach (var tilePos in shuffledTiles)
             {
