5f34a66e9b2679451c2789bb7eeec3035141294f
Murray Groves <github@murraygrov.es>
2025-03-08 20:12:52 +0000

    Add total rats, hordes, players, to leaderboard

--- leaderboard/src/routes/+page.svelte
+++ leaderboard/src/routes/+page.svelte
@@ -16,8 +16,8 @@
 	</Marquee>
 	<div class="flex">
 		<div class="flex-1 p-5 block">
-			<h1 class="text-3xl pt-30 underline mx-auto text-center mb-5">Leaderboard</h1>
-			<table class="w-full border-2 mx-auto text-left">
+			<h1 class="text-3xl pt-3 underline mx-auto text-center mb-5">Current</h1>
+			<table class="w-full border-2 mx-auto text-left h-5/6">
 				<thead>
 				<tr>
 					<th class="p-3">Rank</th>
@@ -41,21 +41,31 @@
 					</tr>
 				{/each}
 				</tbody>
-
-
 			</table>
 		</div>
 		<div class="flex-1 flex-row min-h-screen">
 			<div class="flex-1 p-10">
 				<img src="/pestis.png" alt="Pestis" class="w-64 aspect-square mx-auto" />
 			</div>
-			<div class="flex-1 flex justify-center">
+			<div class="flex-1 text-center">
 				<h1 class="text-4xl">Rat-based Domination!</h1>
+				<h2 class="mt-5 text-2xl">
+					Current
+					Rats: {leaderboard.map(player => player.hordes.map(horde => horde.rats).reduce((sum, rats) => sum + rats)).reduce((sum, rats) => sum + rats)}
+				</h2>
+				<h2 class="mt-5 text-2xl">
+					Current
+					Hordes: {leaderboard.map(player => player.hordes.length).reduce((sum, hordes) => sum + hordes)}
+				</h2>
+				<h2 class="mt-5 text-2xl">
+					Current
+					Players: {leaderboard.length}
+				</h2>
 			</div>
 		</div>
 
 		<div class="flex-1 p-5 flex justify-center">
-			<h1 class="text-3xl pt-30 underline">Highlighted Player</h1>
+			<h1 class="text-3xl pt-3 underline">Best of Today</h1>
 		</div>
 	</div>
 	<Marquee direction="left" class="pb-3 absolute bottom-15">
