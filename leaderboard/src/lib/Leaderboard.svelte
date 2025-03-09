4308cfd10bbc7768a5e31783515f673f1c281cb2
Murray Groves <github@murraygrov.es>
2025-03-09 10:21:19 +0000

    Rework table to look nicer

--- leaderboard/src/lib/Leaderboard.svelte
+++ leaderboard/src/lib/Leaderboard.svelte
@@ -4,28 +4,24 @@
 	export let leaderboard: Player[] = [];
 </script>
 
-<table class="w-full border-2 mx-auto text-left h-5/6 overflow-scroll">
-	<thead>
-	<tr>
-		<th class="p-3">Rank</th>
-		<th>Username</th>
-		<th>Score</th>
-		<th>Rats</th>
-		<th>Hordes</th>
-		<th>POIs</th>
-	</tr>
-	</thead>
-	<tbody>
-	{#each leaderboard as player, i (player.id)}
-		<tr>
-			<td class="p-3">{i + 1}.</td>
-			<td>{player.username}</td>
-			<td>{player.score}</td>
-			<td>{player.hordes.length === 0 ? 0 : player.hordes.map(horde => horde.rats).reduce((sum, rats) => sum + rats)}</td>
-			<td>{player.hordes.length}</td>
-			<td>{player.pois.length}</td>
+<div class="w-full border-2 mx-auto text-left h-5/6 overflow-scroll">
+	<div class="h-10 align-baseline flex w-full font-bold text-left pl-3">
+		<p class="flex-1 pr-1">Rank</p>
+		<p class="flex-3">Username</p>
+		<p class="flex-2">Score</p>
+		<p class="flex-2">Rats</p>
+		<p class="flex-2">Hordes</p>
+		<p class="flex-2">POIs</p>
+	</div>
 
-		</tr>
+	{#each leaderboard as player, i (player.id)}
+		<div class="align-baseline flex text-left pl-3 {i % 2 === 0 ? 'text-gray-50': 'text-slate-300'}">
+			<p class="flex-1 pr-1">{i + 1}.</p>
+			<p class="flex-3">{player.username}</p>
+			<p class="flex-2">{player.score}</p>
+			<p class="flex-2">{player.hordes.length === 0 ? 0 : player.hordes.map(horde => horde.rats).reduce((sum, rats) => sum + rats)}</p>
+			<p class="flex-2">{player.hordes.length}</p>
+			<p class="flex-2">{player.pois.length}</p>
+		</div>
 	{/each}
-	</tbody>
-</table>
+</div>
