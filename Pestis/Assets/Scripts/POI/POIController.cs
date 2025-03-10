01fd646074579a48bc485830c88686bd5bd23416
Shayaan Ahmed <117999566+shayaan-ahm3d@users.noreply.github.com>
2025-03-10 00:19:40 +0000

    capture POI and split horde objectives work

--- Pestis/Assets/Scripts/POI/POIController.cs
+++ Pestis/Assets/Scripts/POI/POIController.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using Fusion;
 using Horde;
 using JetBrains.Annotations;
+using Objectives;
 using Players;
 using UnityEditor;
 using UnityEngine;
@@ -67,6 +68,10 @@ Stationed: {string.Join("\n    ", StationedHordes.Select(x => x.Object.Id))}
             Debug.Log($"Fixed cheese rate is {_cheesePerTick}");
             ControlledBy.IncrementCheeseIncrementRateRpc(_cheesePerTick);
             StationedHordes.Clear();
+            if (player.IsLocal)
+            {
+                GameManager.Instance.ObjectiveManager.AddProgress(ObjectiveTrigger.POICaptured, 1);
+            }
         }
 
         public void EmitCaptureEffect()
