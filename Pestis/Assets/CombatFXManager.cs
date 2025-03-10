f4f86016fac0c2639c691c9823e943ecdcb10bf7
VinhBui01 <15buiv@johnmasonschool.org>
2025-03-10 16:33:12 +0000

    only intersection smoke for real this time
    initiator player calculates intersection and spawns smoke

--- Pestis/Assets/CombatFXManager.cs
+++ Pestis/Assets/CombatFXManager.cs
@@ -1,4 +1,6 @@
+using Horde;
 using System.Collections;
+using System.Linq;
 using UnityEngine;
 
 public class CombatFXManager : MonoBehaviour
@@ -37,19 +39,51 @@ public class CombatFXManager : MonoBehaviour
         endnoise = true;
     }
 
+
+    Bounds GetIntersection(Bounds a, Bounds b)
+    {
+        Vector3 min = Vector3.Max(a.min, b.min); // Maximum of the min corners
+        Vector3 max = Vector3.Min(a.max, b.max); // Minimum of the max corners
+
+        if (min.x > max.x || min.y > max.y || min.z > max.z)
+        {
+            // No valid intersection
+            return new Bounds(Vector3.zero, Vector3.zero);
+        }
+
+        Vector3 center = (min + max) * 0.5f;
+        Vector3 size = max - min;
+
+        return new Bounds(center, size);
+    }
     void UpdateParticleBounds(Bounds bounds)
     {
         // Configure the Shape Module
         var shape = combatVFX.shape;
         combatVFX.transform.position = bounds.center;
-        shape.scale = bounds.size/2;
-        float radius = (bounds.size.x) + 0.5f;
-        combatVFX.emissionRate =  (radius * radius) * 0.7f ;
+        shape.scale = bounds.size;
+        float radius = bounds.extents.magnitude + 0.5f;
+        combatVFX.emissionRate =  (radius * radius) * 1.5f ;
+    }
+
+    Bounds getHordeBounds()
+    {
+        var hordelist = HordeController.CurrentCombatController.GetHordes().Where(h => h != HordeController).ToList();
+        Bounds SumBound = new Bounds(hordelist[0].GetBounds().center, hordelist[0].GetBounds().size);
+        foreach (HordeController horde in hordelist)
+        {
+            SumBound.Encapsulate(horde.GetBounds());
+        }
+        return SumBound;
     }
 
     void Update()
     {
-        UpdateParticleBounds(HordeController.CurrentCombatController.bounds);
+        if (!HordeController.CurrentCombatController.HordeIsVoluntary(HordeController))
+        {
+            UpdateParticleBounds(GetIntersection(getHordeBounds(), HordeController.GetBounds()));
+
+        }
     }
     private void OnEnable()
     {
