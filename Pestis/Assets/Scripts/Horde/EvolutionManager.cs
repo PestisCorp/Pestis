c17a53989ae593a859dc91210ef371259d21a704
AniBoja <aniboja26@gmail.com>
2025-03-12 11:31:56 +0000

    Resize speech bubbles.

--- Pestis/Assets/Scripts/Horde/EvolutionManager.cs
+++ Pestis/Assets/Scripts/Horde/EvolutionManager.cs
@@ -254,7 +254,7 @@ namespace Horde
             _evolutionaryState.PassiveEvolutions["attack"] = new []{0.03, _populationController.GetState().Damage, 2.0};
             _evolutionaryState.PassiveEvolutions["health"] = new []{0.03, _populationController.GetState().HealthPerRat, 20.0};
             _evolutionaryState.PassiveEvolutions["defense"] = new []{ 0.03, _populationController.GetState().DamageReduction, 2.5};
-            _evolutionaryState.PassiveEvolutions["evolution rate"] = new []{ 0.01, 8, 0.5};
+            _evolutionaryState.PassiveEvolutions["evolution rate"] = new []{ 0.01, 4, 0.5};
             _evolutionaryState.PassiveEvolutions["evolution strength"] = new []{ 0.01, 1.02, 1.3};
             _evolutionaryState.PassiveEvolutions["birth rate"] = new[]{ 0.01, _populationController.GetState().BirthRate, 0.1};
             //_evolutionaryState.PassiveEvolutions["resource consumption"] = new []{ 0.0005, _hordeController.Player.CheeseIncrementRate };
