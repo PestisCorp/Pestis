0d41a2cc52137602d5b7bb6aefc9d8d196e6f4e2
VinhBui <117273239+VinhAnBui@users.noreply.github.com>
2025-03-13 16:43:26 +0000

    should be more efficient

--- Pestis/Assets/BattleHPBarManager.cs
+++ Pestis/Assets/BattleHPBarManager.cs
@@ -5,17 +5,15 @@ public class BattleHPBarManager : MonoBehaviour
 {
     public Horde.HordeController horde;
     public Slider HPSlider;
-    private float full;
-    private float currentfill;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void OnAwake()
     {
-        full = horde.TotalHealth;
+        HPSlider.maxValue= horde.TotalHealth;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        HPSlider.value = currentfill/full;
+        HPSlider.value = horde.TotalHealth;
     }
 }
