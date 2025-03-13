05bba79c74cbd686a3205ebe98d18ed56d962530
VinhBui <117273239+VinhAnBui@users.noreply.github.com>
2025-03-13 17:15:05 +0000

    works not limited to local

--- Pestis/Assets/BattleHPBarManager.cs
+++ Pestis/Assets/BattleHPBarManager.cs
@@ -5,15 +5,17 @@ public class BattleHPBarManager : MonoBehaviour
 {
     public Horde.HordeController horde;
     public Slider HPSlider;
+    //private float full;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void OnAwake()
+    void OnEnable()
     {
-        HPSlider.maxValue= horde.TotalHealth;
+        HPSlider.maxValue = horde.TotalHealth;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        //Debug.Log(horde.name+" " + HPSlider.maxValue + "  "+ horde.TotalHealth);
         HPSlider.value = horde.TotalHealth;
     }
 }
