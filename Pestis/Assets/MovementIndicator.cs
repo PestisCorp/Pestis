ae8458266a5f6285050a58aecf09acf34a2b7ba2
VinhBui <117273239+VinhAnBui@users.noreply.github.com>
2025-03-24 18:05:19 +0000

    just looks nicer

--- Pestis/Assets/MovementIndicator.cs
+++ Pestis/Assets/MovementIndicator.cs
@@ -1,4 +1,5 @@
 using Horde;
+using ProceduralToolkit;
 using ProceduralToolkit.LibTessDotNet;
 using UnityEngine;
 using UnityEngine.InputSystem.XR;
@@ -10,13 +11,16 @@ public class MovementIndicator : MonoBehaviour
     public LineRenderer lineRenderer;
     private void FixedUpdate()
     {
-        if (controller.Player.IsLocal)
+        if (controller.Player.IsLocal )
         {
+            if ((controller.GetCenter().ToVector3XY() - controller.targetLocation.transform.position).magnitude < controller.GetBounds().size.magnitude / 2)
+            { lineRenderer.enabled = false; return; }
+
             lineRenderer.enabled = true;
             lineRenderer.positionCount = 2;
             Vector3 position = controller.targetLocation.transform.position;
             Vector3 center  = controller.GetCenter();
-            var radius = controller.GetBounds().size.magnitude/2;
+            float radius = controller.GetBounds().size.magnitude/2 + 0.5f;
             Vector3 direction = (position - center).normalized;
 
             Vector3[] positions = new Vector3[] { center + direction * radius, position };
