73b43b27d3a56fa14b6ac06e6a294e885d69a0ca
Murray Groves <github@murraygrov.es>
2025-03-29 15:26:01 +0000

    Disable movement indicator for first second to hide visual glitch

--- Pestis/Assets/Scripts/UI/MovementIndicator.cs
+++ Pestis/Assets/Scripts/UI/MovementIndicator.cs
@@ -1,5 +1,4 @@
 using Horde;
-using ProceduralToolkit;
 using UnityEngine;
 
 public class MovementIndicator : MonoBehaviour
@@ -7,26 +6,39 @@ public class MovementIndicator : MonoBehaviour
     public HordeController controller;
     public LineRenderer lineRenderer;
 
+    private bool _disabled = true;
+
+    private void Start()
+    {
+        if (controller.player.IsLocal) Invoke(nameof(Enable), 1);
+    }
+
     private void FixedUpdate()
     {
-        if (controller.player.IsLocal)
+        if (_disabled) return;
+
+        if (controller.GetBounds().center.x == 0 || controller.targetLocation.transform.position.x == 0) return;
+
+        if ((controller.GetBounds().center - controller.targetLocation.transform.position).magnitude <
+            controller.GetBounds().size.magnitude / 2)
         {
-            if ((controller.GetCenter().ToVector3XY() - controller.targetLocation.transform.position).magnitude <
-                controller.GetBounds().size.magnitude / 2)
-            {
-                lineRenderer.enabled = false;
-                return;
-            }
-
-            lineRenderer.enabled = true;
-            lineRenderer.positionCount = 2;
-            var position = controller.targetLocation.transform.position;
-            Vector3 center = controller.GetCenter();
-            var radius = controller.GetBounds().size.magnitude / 2 + 0.5f;
-            var direction = (position - center).normalized;
-
-            var positions = new[] { center + direction * radius, position };
-            lineRenderer.SetPositions(positions);
+            lineRenderer.enabled = false;
+            return;
         }
+
+        lineRenderer.enabled = true;
+        lineRenderer.positionCount = 2;
+        var position = controller.targetLocation.transform.position;
+        var center = controller.GetBounds().center;
+        var radius = controller.GetBounds().size.magnitude / 2 + 0.5f;
+        var direction = (position - center).normalized;
+
+        var positions = new[] { center + direction * radius, position };
+        lineRenderer.SetPositions(positions);
+    }
+
+    private void Enable()
+    {
+        _disabled = false;
     }
 }
